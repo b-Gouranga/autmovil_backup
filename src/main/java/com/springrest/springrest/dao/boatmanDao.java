5298c961cd9ccc1a94bacaa7d6d3c8aa6a1e343b
Gouranga Borah <gourangaborahdhun5@gmail.com>
2023-10-17 15:55:32 +0530

    do changes

--- src/main/java/com/springrest/springrest/dao/boatmanDao.java
+++
@@ -1,9 +0,0 @@
-package com.springrest.springrest.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springrest.springrest.entities.Boatman;
-
-public interface boatmanDao  extends JpaRepository<Boatman,Long> {
-
-}
--- src/main/java/com/springrest/springrest/dao/tripDao.java
+++
@@ -1,9 +0,0 @@
-package com.springrest.springrest.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springrest.springrest.entities.Trip;
-
-public interface tripDao extends JpaRepository<Trip,Long> {
-	
-}
